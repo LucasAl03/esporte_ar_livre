665cd5b8aca13cf5a7086ecb590029d6c2a5b085
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-12 20:34:06 -0300

    Projeto inicializado

---
+++ src/app/component/cadastro-atleta/cadastro-atleta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-atleta',
+  imports: [],
+  templateUrl: './cadastro-atleta.html',
+  styleUrl: './cadastro-atleta.css',
+})
+export class CadastroAtleta {}
