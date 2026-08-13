ee57b127b508546e8d946c4017e3845e1a354e47
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-12 21:38:23 -0300

    paths para os componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,21 @@
 import { Routes } from '@angular/router';
 
+import { HomeComponent } from './component/home-component/home-component';
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+
+
 export const routes: Routes = [
-    
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'cadastroatleta',
+        component: AtletaComponent
+    }
 ];
