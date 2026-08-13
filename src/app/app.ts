7ec23c9b71ed891f91b4f77a02f29d0879cf68a6
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-12 21:14:43 -0300

    Import de atletacomponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+
 import { Menu } from './component/menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, Menu, AtletaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
