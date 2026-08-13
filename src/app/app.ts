f0e9d3d2eef4fd6952bc710b330495e5894cbe65
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-13 19:04:51 -0300

    remoção do import de atletacomponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { AtletaComponent } from './component/atleta-component/atleta-component';
-
 import { Menu } from './component/menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu, AtletaComponent],
+  imports: [RouterOutlet, Menu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
