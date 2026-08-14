568cef985cb5e8b178b6d95ff3af574ed806bf0c
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-14 20:52:57 -0300

    Rota para o component cadastroCorrida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,25 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home-component/home-component';
 import { AtletaComponent } from './component/atleta-component/atleta-component';
 
+import { CorridaComponent } from './component/corrida-component/corrida-component';
+
 
 export const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
     },
     {
-        path: 'home',
+        path: "home",
         component: HomeComponent
     },
     {
-        path: 'cadastroatleta',
+        path: "cadastrarAtleta",
         component: AtletaComponent
+    },
+    {
+        path: "cadastroCorrida",
+        component: CorridaComponent
     }
 ];
