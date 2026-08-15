30b18034dae73c7306badde31d66a7d686cab11e
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-14 21:34:01 -0300

    Criado o component listaCorridas

---
+++ src/app/component/lista-corridas/lista-corridas.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CorridaCadastroService } from '../../service/corrida-cadastro-service';
+
+
+
+@Component({
+  selector: 'app-lista-corridas',
+  imports: [],
+  templateUrl: './lista-corridas.html',
+  styleUrl: './lista-corridas.css',
+})
+export class ListaCorridas {
+  constructor(private cadastroService: CorridaCadastroService){}
+
+  listar(){
+    return this.cadastroService.listarCorrida()
+  }
+}
