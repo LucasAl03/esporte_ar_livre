0caec632ee7ee3059effc8ae0267c8f3841f199b
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-17 21:25:00 -0300

    Adicionado o codigo do professor

--- src/app/component/atleta-component/atleta-component.ts
+++ src/app/component/atleta-component/atleta-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AtletaService } from '../../service/atleta-service';
@@ -51,7 +51,7 @@ export class AtletaComponent {
     pessoaAtleta.endereco.uf = this.uf
     */
 
-    this.atletaService.adicionarAtleta(pessoaAtleta)
+    /*this.atletaService.adicionarAtleta(pessoaAtleta)
 
     this.atletaService.listarAtleta()
 
@@ -59,6 +59,138 @@ export class AtletaComponent {
   }
 
   limparAtributos(){
+    this.nome = ''
+    this.cpf = ''
+    this.sexo = ''
+    this.cep = ''
+    this.ruaLogradouro = ''
+    this.bairro = ''
+    this.cidade = ''
+    this.uf = ''
+  }
+}*/
+
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AtletaService } from '../../service/atleta-service';
+import { Atleta } from '../../models/Pessoa';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-atleta-component',
+  imports: [FormsModule],
+  templateUrl: './atleta-component.html',
+  styleUrl: './atleta-component.css',
+})
+export class AtletaComponent {
+  //DELCARAÇÃO DOS ATRIBUTOS DO COMPONENTE
+  id = 0
+  nome = ''
+  cpf = ''
+  sexo = ''
+  cep = ''
+  ruaLogradouro = ''
+  bairro = ''
+  cidade = ''
+  uf = ''
+
+  editar = false
+  idAtleta = 0
+
+  //DECLARAÇÃO DO CONSTRUTOR  
+  constructor(private atletaService: AtletaService, private route: ActivatedRoute, private cdr: ChangeDetectorRef) { }
+
+  //DECLARAÇÃO DE FUNÇÕES
+  exibeDados() {
+    console.log(this.nome, this.cpf, this.sexo, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
+  }
+
+  ngOnInit() {
+    this.idAtleta = Number(this.route.snapshot.paramMap.get('id'))
+
+    if (this.idAtleta > 0) {
+      this.editar = true
+      this.carregaCampo(this.idAtleta)
+    }
+
+  }
+
+  carregaCampo(idAtleta: number) {
+    this.atletaService.listarAtleta(idAtleta)
+      .subscribe({
+        next: (objAtleta) => {
+          this.id = objAtleta.id
+          this.nome = objAtleta.nome
+          this.cpf = objAtleta.cpf
+          this.sexo = objAtleta.sexo
+          this.cep = objAtleta.cep
+          this.ruaLogradouro = objAtleta.ruaLogradouro
+          this.bairro = objAtleta.bairro
+          this.cidade = objAtleta.cidade
+          this.uf = objAtleta.uf
+
+          this.cdr.detectChanges()
+        }, error: (msgErro) => {
+          console.log("Erro ao Listar  o atleta ", msgErro)
+        }
+      })
+  }
+
+  enviaDadosAtleta() {
+    const pessoaAtleta = new Atleta()
+    pessoaAtleta.nome = this.nome
+    pessoaAtleta.cpf = this.cpf
+    pessoaAtleta.sexo = this.sexo
+    pessoaAtleta.cep = this.cep
+    pessoaAtleta.ruaLogradouro = this.ruaLogradouro
+    pessoaAtleta.bairro = this.bairro
+    pessoaAtleta.cidade = this.cidade
+    pessoaAtleta.uf = this.uf
+
+    if (!this.editar) {
+      this.atletaService.adicionarAtleta(pessoaAtleta)
+        .subscribe({
+          next: (resposta) => {
+            console.log(resposta)
+          },
+          error: (msgErro) => {
+            console.log("Erro ao cadastrar  o atleta ", msgErro)
+          }
+        })
+    } else {
+      pessoaAtleta.id = this.idAtleta
+      
+      this.atletaService.alterarAtleta(pessoaAtleta)
+        .subscribe({
+          next: (resposta) => {
+            console.log(pessoaAtleta)
+
+            console.log(resposta)
+          },
+          error: (msgErro) => {
+            console.log("Erro ao alterar  o atleta ", msgErro)
+          }
+        })
+
+    }
+
+    this.limparAtributos()
+
+  }
+
+  listaAtleta(idAtleta: number) {
+    this.atletaService.listarAtleta(idAtleta)
+      .subscribe({
+        next: (dados) => {
+          console.table(dados)
+        },
+        error: (msgErro) => {
+          console.log("Erro ao listar atletas ", msgErro)
+        }
+      })
+  }
+
+  limparAtributos() {
     this.nome = ''
     this.cpf = ''
     this.sexo = ''
