b8ab6fb13cf7aee3d2cf6b68c438fb9a3945a6f3
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-19 19:49:18 -0300

    Funções para atualizar a pagina a cada mudança, para buscar o id do objeto para edição, para excluir o objeto e para listar os objetos

--- src/app/component/lista-corridas/lista-corridas.ts
+++ src/app/component/lista-corridas/lista-corridas.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CorridaCadastroService } from '../../service/corrida-cadastro-service';
+import { Corrida } from '../../models/corrida';
+import { Router } from '@angular/router';
 
 
 
@@ -10,9 +12,46 @@ import { CorridaCadastroService } from '../../service/corrida-cadastro-service';
   styleUrl: './lista-corridas.css',
 })
 export class ListaCorridas {
-  constructor(private cadastroService: CorridaCadastroService){}
+  constructor(private http: CorridaCadastroService, private route: Router){}
 
-  listar(){
-    return this.cadastroService.listarCorridas()
+  listaCorridas = signal<Corrida[]>([])
+
+  ngOnInit(){
+    this.listarCorridas()
+  }
+
+  listarCorridas(){
+    this.http.listarCorridas()
+      .subscribe({
+        next: (dados) => {
+          this.listaCorridas.set([...dados].sort((a, b) => a.descricaoCorrida.localeCompare(b.descricaoCorrida)))
+        },
+        error: (msgErro) => {
+          console.log("Erro ao cadastrar a corrida", msgErro)
+        }
+      })
+  }
+
+  excluirCorrida(corrida: Corrida){
+    if(confirm(`Deseja excluir ${corrida.descricaoCorrida} da lista?`)){
+      this.http.excluirCorrida(corrida)
+        .subscribe({
+          next:(dados)=>{
+            this.listaCorridas.update(elem =>
+              elem.filter(a => a.id !== corrida.id)
+            );
+
+            console.log('Corrida excluída!', dados)
+          },
+          error: (msgErro) => {
+            console.log('Erro ao excluir a corrida!', msgErro)
+          }
+        })
+    }
+    this.ngOnInit()
+  }
+
+  buscaCorrida(idCorrida: Corrida){
+    this.route.navigate(['cadastroCorrida', idCorrida])
   }
 }
